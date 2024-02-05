205d036edaa095bcec0136c44947460ac6244c36
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-05 21:25:24 +0000

    sdfsdf

--- DataTypes.java
+++ DataTypes.java
@@ -3,5 +3,8 @@ public class DataTypes
     public static void main(String [] args)
     {
         int a = 5;
+        double b,c=5.2;
+        boolean g = true;
+        System.out.println("a: " + a + "\t b: " + b + "\t c; " + c);
     }
 }
