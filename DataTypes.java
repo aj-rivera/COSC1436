454570612a06aa387f3142d8219c4fe323345d6c
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-05 21:31:51 +0000

    yep

--- DataTypes.java
+++ DataTypes.java
@@ -3,8 +3,9 @@ public class DataTypes
     public static void main(String [] args)
     {
         int a = 5;
-        double b, c=5.2;
+        double b=5.2;
+        double c=5.2;
         boolean g = true;
-        System.out.println("a: " + a + "\t b: " + b + "\t c; " + c);
+        System.out.println("a: " + a + "\t b: " + b + "\t c; " + c + "\t g:" + g);
     }
 }
