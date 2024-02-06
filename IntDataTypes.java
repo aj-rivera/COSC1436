3bbdbda27bf3e766e843c7eca29f2e39428c27c3
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-06 01:42:25 +0000

    new file

---
+++ IntDataTypes.java
@@ -0,0 +1,13 @@
+public class IntDataTypes
+{
+    public static void main(String[] args)
+    {
+        byte x = 1;
+        
+        while(x>0)
+        {
+            x+= 10;
+            System.out.println(x);
+        }
+    }
+}
