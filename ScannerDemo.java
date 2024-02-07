d3b7e99bdad72641091dca7b12f0ce74403b3438
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-07 20:37:17 +0000

    scanner demo

---
+++ ScannerDemo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+    public static void main(String [] args)
+    {
+        //declarations
+        String name;
+        double favNum;
+
+        //obtaing input from user
+        Scanner kb = new Scanner(System.in);
+        System.out.println("What's your name?");
+        name = kb.nextLine();
+        System.out.println("What's your fave number?");
+        favNum = kb.nextDouble();
+
+        //processing input
+        favNum *= 2;
+
+        //output
+        System.out.println(name + ", your fave number multiplied by 2 is " + favNum);
+    }
+}
