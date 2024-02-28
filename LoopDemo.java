92735f23b6cf4af576c949eb9bd56d23d1121186
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-28 00:11:09 +0000

    idk

---
+++ LoopDemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class LoopDemo
+{
+    public static void main(String [] args)
+    {
+        
+        Scanner kb = new Scanner(System.in)
+        String x =kb.next();
+
+        do
+        {
+            System.out.println("running...");
+            x=kb.next();
+        } while (x.equals("n"))
+    }
+}
