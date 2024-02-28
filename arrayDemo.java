ba8a24c93758f8495d5738d964e4d5f9404cbcbd
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-02-28 00:43:53 +0000

    fsdf

--- arrayDemo.java
+++ arrayDemo.java
@@ -2,13 +2,12 @@ public class arrayDemo
 {
     public static void main(String[] args)
     {
-        String [] students = new String[5];
-        students[0]="Ronnie";
-        students[1]="Chris";
-        students[2]="Robin";
-        students[3]="Lorrie";
-        students[4]="Kevin";
+        String [] students = {"Ronnie", "Chris", "Robin","Lorrie","Kevin"}
         System.out.println(students);
+        for (i=0, i<students.lengths-1;i++)
+        {
+            System.out.print(students[i]+",")
+        }
         
     }
 }
