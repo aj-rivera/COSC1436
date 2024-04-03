940b95b3c182e497e081f5850295d2ae09e1a134
aj-rivera <142535929+aj-rivera@users.noreply.github.com>
2024-04-03 15:52:25 +0000

    organizing comments

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class bubbleSort {
+    // declarations
     private static int totalSwaps;
     private static int totalComparisons;
     private static int swaps;
@@ -8,22 +9,21 @@ public class bubbleSort {
     private static int pass;
     private static final int LIST_LENGTH=5;
     public static void main(String args[]) {
+        
         //creating variables
         ArrayList<Integer> numbers = new ArrayList<Integer>();        
         totalSwaps=0;
         totalComparisons=0;
         
-        // making array of random numbers
-        generateArray(numbers);
+        generateArray(numbers); // making array of random numbers
         
-        // printing original array
-        System.out.println("Original Array: " + numbers);
+        System.out.println("Original Array: " + numbers);   // printing original array
         
-        //initiating comparison count
-        comparisons=numbers.size()-1;
         
-        // loop where a bubble passthrough is made and results are printed
-        // accumulators totalSwaps, totalComparisons are added to as well
+        comparisons=numbers.size()-1;   //initiating comparison count
+        
+        /* loop where a bubble passthrough is made and results are 
+        printed accumulators totalSwaps, totalComparisons are added to as well */ 
         for (pass=1; pass<=numbers.size()-2; pass++) {
             bubblePass(numbers);
             displayBubblePass(numbers);
@@ -32,8 +32,7 @@ public class bubbleSort {
             comparisons--;
         }
 
-        // ending array and accumulators displayed
-        displaySortedArray(numbers);
+        displaySortedArray(numbers);    // ending array and accumulators displayed
     }
     
 
@@ -44,14 +43,15 @@ public class bubbleSort {
         }
     }
 
+
     //restarts swaps number and then does a bubble sort passthrough
     public static void bubblePass(ArrayList<Integer> numbers) {
         swaps=0;
         for (int index=0; index<=numbers.size()-2;index++) {
-            // if a number is larger than the one that proceeds it,
-            // it will switch the numbers
-            if (numbers.get(index)>numbers.get(index+1)) {
-                int temp = numbers.get(index);
+            
+            
+            if (numbers.get(index)>numbers.get(index+1)) {  // checks if # is larger than the one that proceeds it,
+                int temp = numbers.get(index);              // it will switch the numbers
                 numbers.set(index, numbers.get(index+1));
                 numbers.set(index+1, temp);
                 swaps++;
@@ -60,14 +60,16 @@ public class bubbleSort {
     }
 
 
-    // method that displays array after each bubble sort passthrough
-    // and the number of comparisons and swaps during that passthrough
+    /* method that displays array after each bubble sort passthrough
+        and the number of comparisons and swaps during that passthrough
+    */
     public static void displayBubblePass(ArrayList<Integer> numbers) {
         System.out.print("Pass " + pass+ ":" +numbers);
         System.out.print(" (Comparisons: " + comparisons);
         System.out.println(", Swaps: "+ swaps + ")");
         
     }
+
 
     // method that displays finished array and accumulators
     public static void displaySortedArray(ArrayList<Integer> numbers) {
@@ -76,6 +78,4 @@ public class bubbleSort {
         System.out.println(" Total Swaps: "+ totalSwaps);
         
     }
-
-        
 }
